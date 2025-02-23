5cafb977a17bb4405e550f7cd38cc4ff85791cc1
OrkunDmrc <orkundemircit147@gmail.com>
2025-02-23 15:54:11 +0100

    feat - unsupervised learning

---
+++ MachineLearningPractices/UnsupervisedLearning/5_StateStudy.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+data = pd.read_pickle("UnsupervisedLearning/5_StateStudy")
+X = data.values
+X[:,0] = np.abs(2*min(X[:,0]) + X[:,0])
+X[:,1] = np.abs(2*min(X[:,1]) + X[:,1])
+
+plt.figure()
+plt.scatter(X[:,0],X[:,1],s=50,alpha=0.7,edgecolors="k")
+plt.title("Customer Segmentation")
+plt.xlabel("income")
+plt.ylabel("spending score")
+plt.show()
+
+kmeans = KMeans(n_clusters=5)
+kmeans.fit(X)
+
+cluster_labels = kmeans.labels_
+cluster_centers = kmeans.cluster_centers_
+
+plt.subplot(1,2,1)
+plt.scatter(X[:,0],X[:,1],c=cluster_labels,s=50,alpha=0.7,edgecolors="k")
+plt.title("KMeans - Cutomer Segmentation")
+plt.xlabel("income")
+plt.ylabel("spending score")
+
+linkage_matrix = linkage(X, method = "ward")
+
+plt.subplot(1,2,2)
+dendrogram(linkage_matrix)
+plt.title("Dendrogram - Cutomer Segmentation")
+plt.xlabel("data points")
+plt.ylabel("distance")
+plt.show()
+
+
+
+
